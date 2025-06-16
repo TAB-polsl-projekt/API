916a0b6ad4debd690d4df58992dbe0aaa62a6b56
Michał Kostrzewski <michal.kostrzewski@protonmail.com>
2025-06-16 21:18:06 +0200

    Fix get student-assignmets

--- src/api/subject_student_assignments_get.rs
+++ src/api/subject_student_assignments_get.rs
@@ -2,8 +2,8 @@ use diesel::{ExpressionMethods, JoinOnDsl, QueryDsl, RunQueryDsl};
 use rocket::{get, serde::json::Json};
 use rocket_okapi::{okapi::openapi3::OpenApi, openapi, openapi_get_routes_spec, settings::OpenApiSettings};
 
-use crate::dbmodels::{Solution};
-use crate::dbschema::{assignments, solution, subjects, user_solution_assignments};
+use crate::dbmodels::{Assignment, Solution};
+use crate::dbschema::{assignments, solution, subjects, user_solution_assignments, user_subjects};
 use crate::define_api_response;
 use crate::session::Session;
 
@@ -12,7 +12,7 @@ pub fn get_routes_and_docs(settings: &OpenApiSettings) -> (Vec<rocket::Route>, O
 }
 
 define_api_response!(pub enum Response {
-    Ok => (200, "Returns user id and solution data", Vec<(String, Solution)>, ()),
+    Ok => (200, "Returns user id and solution data", Vec<(String, Assignment)>, ()),
 });
 
 define_api_response!(pub enum Error {
@@ -20,7 +20,7 @@ define_api_response!(pub enum Error {
 });
 
 #[openapi(tag = "Subjects", operation_id = "aaaa")]
-#[get("/subject/<subject_id>/student-assignments")]
+#[get("/subjects/<subject_id>/student-assignments")]
 pub async fn endpoint(subject_id: String, conn: crate::db::DbConn, session: Session) -> Result<Response, Error> {
     if !session.is_admin {
         return Err(Error::InternalServerError(()));
@@ -29,9 +29,8 @@ pub async fn endpoint(subject_id: String, conn: crate::db::DbConn, session: Sess
     let result = conn.run(move |c| {
         subjects::table
             .inner_join(assignments::table.on(assignments::subject_id.eq(subject_id)))
-            .inner_join(user_solution_assignments::table.on(user_solution_assignments::assignment_id.eq(assignments::assignment_id)))
-            .inner_join(solution::table.on(user_solution_assignments::solution_id.eq(solution::solution_id)))
-            .select((user_solution_assignments::user_id, solution::all_columns))
+            .inner_join(user_subjects::table.on(user_subjects::subject_id.eq(subjects::subject_id)))
+            .select((user_subjects::user_id, assignments::all_columns))
             .get_results(c)
     }).await?;
 
