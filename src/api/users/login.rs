a1a9120e6f4bd38b9cd1aaa8daaf4e0e03e895d9
Michał Kostrzewski <michal.kostrzewski@protonmail.com>
2025-06-29 20:54:03 +0200

    Add old_passwd_hash to PUT /users/{user_id}/login

--- src/api/users/login.rs
+++ src/api/users/login.rs
@@ -1,10 +1,11 @@
+use diesel::dsl::exists;
 use diesel::{AsChangeset, ExpressionMethods, QueryDsl, RunQueryDsl};
 use rocket::{put, serde::json::Json};
 use rocket_okapi::{okapi::openapi3::OpenApi, openapi, openapi_get_routes_spec, settings::OpenApiSettings};
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
-use crate::define_api_response;
+use crate::{define_api_response, define_response_data};
 use crate::schema::logins;
 use crate::session::Session;
 
@@ -12,35 +13,61 @@ pub fn get_routes_and_docs(settings: &OpenApiSettings) -> (Vec<rocket::Route>, O
     openapi_get_routes_spec![settings: put_user_login]
 }
 
-define_api_response!(pub enum PutLoginResponse {
+define_api_response!(enum PutLoginResponse {
     Ok => (200, "Login info updated", (), ())
 });
 
-define_api_response!(pub enum PutLoginError {
+define_api_response!(enum PutLoginError {
+    BadRequest => (400, "Old password is incorrect", (), ()),
     Forbidden => (403, "User cannot modify other users", (), ()),
     InternalServerError => (500, "Failed to update user account", (), (diesel::result::Error)),
 });
 
 #[derive(Debug, Deserialize, Serialize, AsChangeset, JsonSchema)]
 #[diesel(table_name = logins)]
-pub struct LoginUpdate {
+struct LoginUpdate {
     pub email: Option<String>,
-    pub passwd_hash: Option<String>
+    pub passwd_hash: Option<String>,
 }
+
+define_response_data!(
+    struct PutUserLoginRequestData {
+        #[serde(flatten)]
+        pub login_update: LoginUpdate,
+        pub old_passwd_hash: Option<String>
+    }
+);
 
 #[openapi(tag = "Users")]
 #[put("/users/<user_id>/login", data = "<login_update>")]
-pub async fn put_user_login(user_id: String, login_update: Json<LoginUpdate>, conn: crate::db::DbConn, session: Session) -> Result<PutLoginResponse, PutLoginError> {
+async fn put_user_login(user_id: String, login_update: Json<PutUserLoginRequestData>, conn: crate::db::DbConn, session: Session) -> Result<PutLoginResponse, PutLoginError> {
     let login_update = login_update.0;
-
-    if user_id != session.user_id && !session.is_admin {
-        return Err(PutLoginError::Forbidden(()));
-    }
+    let old_passwd_hash = login_update.old_passwd_hash;
+    let login_update = login_update.login_update;
 
     conn.run(move |c| {
+        if !session.is_admin {
+            if user_id != session.user_id {
+                return Err(PutLoginError::Forbidden(()));
+            }
+
+            let old_passwd_hash = old_passwd_hash.ok_or(PutLoginError::BadRequest(()))?;
+            let is_old_password_correct: bool = diesel::select(exists(
+                logins::table
+                    .filter(logins::user_id.eq(&user_id))
+                    .filter(logins::passwd_hash.eq(&old_passwd_hash))
+            )).get_result(c)?;
+
+            if !is_old_password_correct {
+                return Err(PutLoginError::BadRequest(()));
+            }
+        }
+    
         diesel::update(logins::table.filter(logins::user_id.eq(user_id)))
             .set(&login_update)
-            .execute(c)
+            .execute(c)?;
+
+        Ok(())
     }).await?;
 
     Ok(PutLoginResponse::Ok(()))
--- src/response_data.rs
+++ src/response_data.rs
@@ -3,6 +3,7 @@ macro_rules! define_response_data {
     (
         $(#[$meta:meta])*
         $vis:vis struct $name:ident {
+            $(#[$field_meta:meta])*
             $($field_vis:vis $field_name:ident : $field_ty:ty),* $(,)?
         }
     ) => {
@@ -11,6 +12,14 @@ macro_rules! define_response_data {
         $(#[$meta])*
         #[derive(::serde::Serialize, ::serde::Deserialize, Debug)]
         $vis struct $name {
+            $(#[$field_meta])*
+            $($field_vis $field_name : $field_ty),*
+        }
+
+        $(#[$meta])*
+        #[derive(::serde::Serialize, ::serde::Deserialize, Debug, ::schemars::JsonSchema)]
+        struct Helper {
+            $(#[$field_meta])*
             $($field_vis $field_name : $field_ty),*
         }
 
@@ -20,31 +29,12 @@ macro_rules! define_response_data {
             }
 
             fn json_schema(r#gen: &mut SchemaGenerator) -> Schema {
-                let mut obj = ::schemars::schema::ObjectValidation::default();
-
-                $(
-                    obj.properties.insert(
-                        stringify!($field_name).to_string(),
-                        r#gen.subschema_for::<$field_ty>(),
-                    );
-                )*
-
-                // Optional: add required field tracking
-                $(
-                    obj.required.insert(stringify!($field_name).to_string());
-                )*
-
-                let schema = ::schemars::schema::SchemaObject {
-                    instance_type: Some(::schemars::schema::SingleOrVec::Single(Box::new(::schemars::schema::InstanceType::Object))),
-                    object: Some(Box::new(obj)),
-                    metadata: Some(Box::new(::schemars::schema::Metadata {
-                        title: Some(Self::schema_name()),
-                        ..Default::default()
-                    })),
-                    ..Default::default()
-                };
-
-                Schema::Object(schema)
+                // This builds the full “components/schemas/MyType” object
+                let mut root = r#gen.root_schema_for::<Helper>().schema;
+                if let Some(md) = &mut root.metadata {
+                    md.title = Some(Self::schema_name());
+                }
+                Schema::Object(root)
             }
         }
     };
