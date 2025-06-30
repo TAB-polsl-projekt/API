062f9f1d7d1965b68ca722ed9fc0242b37f452f1
Michał Kostrzewski <michal.kostrzewski@protonmail.com>
2025-06-30 18:28:09 +0200

    Improve sending solutions

--- src/api/assignment_solution_post.rs
+++ src/api/assignment_solution_post.rs
@@ -1,6 +1,6 @@
 use chrono::Utc;
-use diesel::dsl::{count_star, exists};
-use diesel::{BelongingToDsl, ExpressionMethods, JoinOnDsl, QueryDsl, RunQueryDsl};
+use diesel::{ExpressionMethods, JoinOnDsl, QueryDsl, RunQueryDsl};
+use itertools::Itertools;
 use rocket::post;
 use rocket::serde::uuid::Uuid;
 use rocket::serde::json::Json;
@@ -8,8 +8,8 @@ use rocket_okapi::{okapi::openapi3::OpenApi, openapi, openapi_get_routes_spec, s
 use crate::{define_api_response, define_response_data};
 use crate::session::Session;
 
-use crate::dbmodels::{Solution, User, UserRole, UserSolution};
-use crate::schema::{assignments, roles, solutions, subject_role, user_role, user_solution, users};
+use crate::dbmodels::{Solution, UserSolution};
+use crate::schema::{assignments, solutions, subject_role, user_role, user_solution};
 
 pub fn get_routes_and_docs(settings: &OpenApiSettings) -> (Vec<rocket::Route>, OpenApi) {
     openapi_get_routes_spec![settings: post_assignment_solution]
@@ -20,13 +20,13 @@ define_api_response!(enum PostAssignmetSolutionResponse {
 });
 
 define_api_response!(enum PostAssignmetSolutionError {
-    BadRequest => (400, "Coauthors are not in the same role as user", (), ()),
-    Forbidden => (403, "User does not belong to the subject", (), ()),
+    BadRequest => (400, "User or co-authors are not in the role", (), ()),
     InternalServerError => (500, "Unexpected server error", (), (diesel::result::Error)),
 });
 
 define_response_data!(struct PostAssignmetSolutionRequestData {
     pub solution: Solution,
+    pub role_id: String,
     pub coauthors_user_ids: Vec<String>
 });
 
@@ -41,38 +41,27 @@ async fn post_assignment_solution(
     let request_data = request_data.0;
 
     let mut sln = request_data.solution;
-    let user_id = session.user_id;
-    let coauthor_ids = request_data.coauthors_user_ids;
+    let user_ids: Vec<String> = request_data.coauthors_user_ids.into_iter()
+        .chain(std::iter::once(session.user_id))
+        .unique()
+        .collect();
+    let role_id = request_data.role_id;
 
     conn.run(move |c| -> Result<_, PostAssignmetSolutionError> {
+        let user_and_coauthors_belong_to_role: bool = {
+            let cnt: i64 = user_role::table
+                .inner_join(subject_role::table.on(subject_role::role_id.eq(user_role::role_id)))
+                .inner_join(assignments::table.on(assignments::subject_id.eq(subject_role::subject_id)))
+                .filter(assignments::assignment_id.eq(&assignment_id))
+                .filter(user_role::role_id.eq(role_id))
+                .filter(user_role::user_id.eq_any(&user_ids))
+                .count()
+                .get_result(c)?;
 
-        let user: User = users::table.find(user_id.clone()).first(c)?;
+            cnt == user_ids.len() as i64
+        };
 
-        let user_assignment_role_ids: Vec<String> = UserRole::belonging_to(&user)
-            .inner_join(roles::table.on(roles::role_id.eq(user_role::role_id)))
-            .inner_join(subject_role::table.on(subject_role::role_id.eq(user_role::role_id)))
-            .inner_join(assignments::table.on(assignments::subject_id.eq(subject_role::subject_id)))
-            .filter(assignments::assignment_id.eq(&assignment_id))
-            .select(roles::role_id)
-            .distinct()
-            .load(c)?;
-
-        let user_has_access_to_subject: bool = !user_assignment_role_ids.is_empty();
-
-        if !user_has_access_to_subject {
-            return Err(PostAssignmetSolutionError::Forbidden(()));
-        }
-
-        let user_and_all_coauthors_have_common_role: bool = diesel::select(exists(
-            user_role::table
-                .filter(user_role::role_id.eq_any(&user_assignment_role_ids))
-                .filter(user_role::user_id.eq_any(&coauthor_ids))
-                .group_by(user_role::role_id)
-                .having(count_star().eq(coauthor_ids.len() as i64)),
-        ))
-        .get_result(c)?;
-
-        if !user_and_all_coauthors_have_common_role {
+        if !user_and_coauthors_belong_to_role {
             return Err(PostAssignmetSolutionError::BadRequest(()))
         }
 
@@ -85,8 +74,7 @@ async fn post_assignment_solution(
             .values(sln)
             .execute(c)?;
 
-        let user_solutions: Vec<_> = coauthor_ids.into_iter()
-            .chain(std::iter::once(user_id))
+        let user_solutions: Vec<_> = user_ids.into_iter()
             .map(|user_id| {
                 UserSolution { user_id, solution_id: solution_id.clone() }
             })
